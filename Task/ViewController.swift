26ee29bf7ba31ee6e1f3c064857b9cf42489772e
Asrbek <asrbekmahmudov>
2021-12-15 10:18:15 +0500

    Initial Commit

---
+++ Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task
+//
+//  Created by Mahmudov Asrbek Ulug'bek o'g'li on 15/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
